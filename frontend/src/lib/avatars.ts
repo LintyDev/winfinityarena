3b9abb0fa2b044bb56fc1c5b227eb9b036332013
Linty <lintydev@gmail.com>
2024-06-19 01:58:33 +0200

    issue #3 add avatars and icons

---
+++ frontend/src/lib/avatars.ts
@@ -0,0 +1,51 @@
+import AvatarObj from '@/types/avatars';
+
+const avatars: AvatarObj[] = [
+  {
+    id: 1,
+    name: 'avatar1',
+    path: '/avatars/avatar1.png',
+  },
+  {
+    id: 2,
+    name: 'avatar2',
+    path: '/avatars/avatar2.png',
+  },
+  {
+    id: 3,
+    name: 'avatar3',
+    path: '/avatars/avatar3.png',
+  },
+  {
+    id: 4,
+    name: 'avatar4',
+    path: '/avatars/avatar4.png',
+  },
+  {
+    id: 5,
+    name: 'avatar5',
+    path: '/avatars/avatar5.png',
+  },
+  {
+    id: 6,
+    name: 'avatar6',
+    path: '/avatars/avatar6.png',
+  },
+  {
+    id: 7,
+    name: 'avatar7',
+    path: '/avatars/avatar7.png',
+  },
+  {
+    id: 8,
+    name: 'avatar8',
+    path: '/avatars/avatar8.png',
+  },
+  {
+    id: 9,
+    name: 'avatar9',
+    path: '/avatars/avatar9.png',
+  },
+];
+
+export default avatars;
