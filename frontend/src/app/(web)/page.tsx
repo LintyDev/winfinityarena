72a729877dfb324888cfd21cb176c854d03ce94a
Linty <lintydev@gmail.com>
2024-06-18 05:21:42 +0200

    issue #2 update footer
    also removed logout function in index page

--- frontend/src/app/(web)/page.tsx
+++ frontend/src/app/(web)/page.tsx
@@ -1,19 +1,6 @@
-'use client';
-
 import StartMenu from '@/components/StartMenu';
-import { useAuth } from '@/contexts/AuthContext';
 
 export default function Home() {
-  const auth = useAuth();
-
-  const logout = async () => {
-    try {
-      auth?.logout();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <main className="flex flex-col items-center">
       <StartMenu />
