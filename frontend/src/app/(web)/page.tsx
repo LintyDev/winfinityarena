491d46fe4498d1a385c2356ba0cf9902285a57ba
Linty <lintydev@gmail.com>
2024-06-19 04:12:36 +0200

    issue #2 add menu title

--- frontend/src/app/(web)/page.tsx
+++ frontend/src/app/(web)/page.tsx
@@ -3,6 +3,7 @@ import StartMenu from '@/components/StartMenu';
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
+      <h1 className="title mt-10">Menu</h1>
       <StartMenu />
     </main>
   );
--- frontend/src/components/StartMenu.tsx
+++ frontend/src/components/StartMenu.tsx
@@ -1,6 +1,6 @@
 function StartMenu() {
   return (
-    <div className="flex flex-col mt-10 gap-4 bg-purple-950 p-5 rounded-md">
+    <div className="flex flex-col gap-4 bg-purple-950 p-5 rounded-md">
       <p className="win-btn green">Créer une session</p>
       <p className="win-btn orange">Classement</p>
     </div>
