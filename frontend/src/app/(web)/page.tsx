de525b0ee0613b40f5b66bab1442b07ecba14877
Linty <lintydev@gmail.com>
2024-06-25 01:41:38 +0200

    issue #5 add prop to StartMenu component
    this prop is for mobile view for startmenu

--- frontend/src/app/(web)/page.tsx
+++ frontend/src/app/(web)/page.tsx
@@ -1,10 +1,12 @@
 import StartMenu from '@/components/StartMenu';
+import { isMobile } from '@/lib/isMobile';
 
 export default function Home() {
+  const mobile = isMobile();
   return (
     <main className="flex flex-col items-center">
       <h1 className="title mt-10">Menu</h1>
-      <StartMenu />
+      <StartMenu isMobile={mobile} />
     </main>
   );
 }
